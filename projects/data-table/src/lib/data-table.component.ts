7a40bb5a2848fda6d6263e65aa2c37cf387e9c91
cedric locchi <cedric.locchi@sosoxygene.com>
2024-10-09 23:29:56 +0200

    first commit

---
+++ projects/data-table/src/lib/data-table.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-dataTable',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      data-table works!
+    </p>
+  `,
+  styles: ``
+})
+export class DataTableComponent {
+
+}
---
+++ projects/data-table/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of data-table
+ */
+
+export * from './lib/data-table.service';
+export * from './lib/data-table.component';
